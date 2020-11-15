c5907a47f6794a4f954b9b91a379b2a3c54d86dd
seif525 <jeddazaseif@gmail.com>
2020-11-15 20:54:18 +0100

    first commit

---
+++ src/Component/profile/ProfilPhoto.js/app.js
@@ -0,0 +1,14 @@
+import src from '*.avif';
+import './App.css';
+
+function App() {
+  return (
+<img src="/download" alt="profile" ></img>
+function profilenam() {
+   let firstname="seif" 
+   let secondenam ="jedda"
+   return (firstname) + (secondenam)
+}
+}
+
+export default App;
